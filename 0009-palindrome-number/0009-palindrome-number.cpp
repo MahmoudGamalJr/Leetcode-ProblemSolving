82c124b0360a9e89000264719d1dbb1bc7b63eac
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-05 05:42:07 +0200

    Time: 14 ms (89.27%), Space: 5.7 MB (99.19%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedNumber = 0;
+        while(x > reversedNumber) 
+        {
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x /= 10;
+        }
+    
+      return x == reversedNumber || x == reversedNumber/10;
+    }
+    
+};
